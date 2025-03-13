8256aacd48e602b13a609013f673b7dc9209d1bc
Aleksandr Konovalov <aleksandr_konovalov@epam.com>
2025-03-14 00:47:17 +0300

    clean up the views

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { setRoutes } from './routes/userRoutes';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import { setRoutes } from "./routes/userRoutes";
+import path from "path";
+import expressLayouts from "express-ejs-layouts";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Setup express-ejs-layouts
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
 
 // Routes
 setRoutes(app);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
